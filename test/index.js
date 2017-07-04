6fb4bcb8c9e72ebe7d6c219598797a06921cd247
Graham Lutz <graham@brainbytescreative.com>
2017-07-04 13:51:19 -0400

    working test for POST new user route /users

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 import http from 'http';
 import assert from 'assert';
 import axios from 'axios';
+import request from 'request';
 
 import '../lib/index.js';
 
@@ -10,5 +11,26 @@ describe('GET all users route - /users', () => {
       assert.equal(200, res.statusCode);
       done();
     });
+  });
+});
+
+describe('POST new user route - /users', () => {
+  it('should return 200', done => {
+  	request.post(
+	    'http://localhost:1701/users',
+	    { json: {
+		    username: 'Test',
+		    email: 'test@example.com',
+		    password: 'testing'
+		  } 
+		},
+	    function (err, res, body) {
+	        if (!err && res.statusCode == 200) {
+	            console.log(body)
+	            assert.equal(200, res.statusCode);
+      			done();
+	        }
+	    }
+	);
   });
 });
