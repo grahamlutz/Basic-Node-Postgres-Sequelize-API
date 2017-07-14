c8d98d007c354b8249334bcc0fb20f5cfb40417f
Graham Lutz <graham@brainbytescreative.com>
2017-07-14 16:56:35 -0400

    removes promise and uses done()

--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import request from 'request';
 
 let url = 'http://localhost:1701/users'
-let userPromise;
+let userID;
 let options =   { json: {
 				    username: 'TestUser',
 				    email: 'test@example.com',
@@ -13,41 +13,37 @@ let options =   { json: {
 				}
 
 describe('GET all users route - /users', () => {
-  it('should return 200', done => {
-    http.get(url, res => {
-      assert.equal(200, res.statusCode);
-      done();
-    });
-  });
+	it('should return 200', done => {
+		http.get(url, res => {
+			assert.equal(200, res.statusCode);
+			done();
+		});
+	});
 });
 
-describe('GET individual users route - /users/:userid', () => {
-  it('should return 200', done => {
-    http.get(url + '/5', res => {
-      assert.equal(200, res.statusCode);
-      done();
-    });
-  });
-});
 
 describe('POST new user route - /users', () => {
 	it('should create new user', done => {
-		userPromise = new Promise( (resolve, reject) => {
-			request.post(url, options, (err, res, body) => {
-		        if (!err && res.statusCode == 200) {
-		            let userID = body.id
-		            assert.equal(200, res.statusCode);
-		  			resolve(userID)
-		  			done()
-		        }
-		    });
-		})
+		request.post(url, options, (err, res, body) => {
+            userID = body.id
+            assert.equal(200, res.statusCode);
+  			done()
+	    });
+	});
+});
+
+describe('GET individual users route - /users/:userid', () => {
+	it('should return 200', done => {
+		http.get(url + '/' + userID, res => {
+			assert.equal(200, res.statusCode);
+			done();
+		});
 	});
 });
 
 describe('PUT update individual user = /users/:userid', () => {
 	it('should update user info', done => {
-		request.put(url + '/5', options, (err, res, body) => {
+		request.put(url + '/' + userID, options, (err, res, body) => {
 			assert.equal(1, res.body);
 			done();
 		})
@@ -56,11 +52,9 @@ describe('PUT update individual user = /users/:userid', () => {
 
 describe('DELETE individual user - /users/:userid', () => {
 	it('should successfully delete Test User', done => {
-		userPromise.then((userID) => {
-			request.delete(url + '/' + userID, (err, res, body) => {
-			        assert.equal(1, res.body);
-			        done()
-				});
-		})
+		request.delete(url + '/' + userID, (err, res, body) => {
+		        assert.equal(1, res.body);
+		        done()
+			});
 	})
 })
