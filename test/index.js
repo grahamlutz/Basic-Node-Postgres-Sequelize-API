c86f0a4821798f1bc806590468e197b8113a0cc4
Graham Lutz <graham@brainbytescreative.com>
2017-07-16 09:24:41 -0400

    formats and organizes test suite

--- test/index.js
+++ test/index.js
@@ -9,60 +9,61 @@ let options =   { json: {
 				    password: 'TestPassword'
 				  } 
 				}
+describe('User Routes', () => {
 
-describe('GET all users route - /users', () => {
-	it('should return 200', done => {
-		request.get(url, (err, res, body) => {
-			assert.equal(200, res.statusCode);
-			done();
+	describe('POST new user route - /users', () => {
+		it('should create new user', done => {
+			request.post(url, options, (err, res, body) => {
+	            userID    = body.id
+	            userEmail = body.email
+	            assert.equal(200, res.statusCode);
+	  			done()
+		    });
 		});
 	});
-});
 
-
-describe('POST new user route - /users', () => {
-	it('should create new user', done => {
-		request.post(url, options, (err, res, body) => {
-            userID    = body.id
-            userEmail = body.email
-            assert.equal(200, res.statusCode);
-  			done()
-	    });
-	});
-});
-
-describe('POST login user route - /users/login', () => {
-	it('should return success message', done => {
-		request.post(url + '/login', options, (err, res, body) => {
-            assert.equal('successful login!', res.body.message);
-  			done()
-	    });
-	})
-})
-
-describe('GET individual users route - /users/:userid', () => {
-	it('should return 200', done => {
-		request.get(url + '/' + userID, (err, res, body) => {
-			assert.equal(200, res.statusCode);
-			done();
-		});
-	});
-});
-
-describe('PUT update individual user = /users/:userid', () => {
-	it('should update user info', done => {
-		request.put(url + '/' + userID, options, (err, res, body) => {
-			assert.equal(1, res.body);
-			done();
+	describe('POST login user route - /users/login', () => {
+		it('should return success message', done => {
+			request.post(url + '/login', options, (err, res, body) => {
+	            assert.equal('successful login!', res.body.message);
+	  			done()
+		    });
 		})
 	})
-})
 
-describe('DELETE individual user - /users/:userid', () => {
-	it('should successfully delete Test User', done => {
-		request.delete(url + '/' + userID, (err, res, body) => {
-		        assert.equal(1, res.body);
-		        done()
+	describe('GET all users route - /users', () => {
+		it('should return 200', done => {
+			request.get(url, (err, res, body) => {
+				assert.equal(200, res.statusCode);
+				done();
 			});
+		});
+	});
+
+	describe('GET individual users route - /users/:userid', () => {
+		it('should return 200', done => {
+			request.get(url + '/' + userID, (err, res, body) => {
+				assert.equal(200, res.statusCode);
+				done();
+			});
+		});
+	});
+
+	describe('PUT update individual user = /users/:userid', () => {
+		it('should update user info', done => {
+			request.put(url + '/' + userID, options, (err, res, body) => {
+				assert.equal(1, res.body);
+				done();
+			})
+		})
+	})
+
+	describe('DELETE individual user - /users/:userid', () => {
+		it('should successfully delete Test User', done => {
+			request.delete(url + '/' + userID, (err, res, body) => {
+			        assert.equal(1, res.body);
+			        done()
+				});
+		})
 	})
 })
